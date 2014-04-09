bd7f70b31b0344d771cd4a8d9de4866e62654eaa
Isaac Rosenberg <irosenb7@gmail.com>
2014-04-08 21:16:56 -0400

    added httparty, starting simple

--- lib/meetup.rb
+++ lib/meetup.rb
@@ -2,12 +2,10 @@ require "meetup/version"
 
 module Meetup
   class Client
-    attr_accessor :access_token, :key
-    def intialize(options={})
-      options.each do |key, value|
-        send(:"#{key}=", value)
-      end
-        yield(self) if block_given?
+    include HTTParty
+
+    
+    def intialize()
     end
   end
 end
