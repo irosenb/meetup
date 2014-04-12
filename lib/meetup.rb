6f776134c630a2f6f96239f1e1358472882d486b
Isaac Rosenberg <irosenb7@gmail.com>
2014-04-11 23:25:22 -0400

    simplified

--- lib/meetup.rb
+++ lib/meetup.rb
@@ -3,9 +3,17 @@ require "meetup/version"
 module Meetup
   class Client
     include HTTParty
+    attr_accessor :access_token, :key
 
-    
-    def intialize()
+    base_uri 'api.meetup.com'
+
+    def initialize(access_token="", key)
+      @access_token = access_token
+      @key = key
+      @options = {:key => @key, :access_token => @access_token}
+
+      self.class.get('/2/self', @options)
     end
+
   end
 end
