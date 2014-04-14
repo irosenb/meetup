f4f0134d2a065df8b038a0f3f15a62b5dbb99fce
Isaac Rosenberg <irosenb7@gmail.com>
2014-04-14 10:00:02 -0400

    only has attr accessor

--- lib/meetup.rb
+++ lib/meetup.rb
@@ -2,20 +2,7 @@ require "meetup/version"
 require "httparty"
 
 module Meetup
-  class Client
-    include HTTParty
-    attr_accessor :key
-    # default_params :key => @key
+  attr_accessor :key, :access_token
 
-    base_uri 'api.meetup.com'
-
-    def initialize(key)
-      @key = key
-      @options = {:query => {:key => @key, :sign => 'true'}}  
-    end
-    
-    def self
-      self.class.get('/2/member/self', @options)  
-    end 
-  end
+  
 end
