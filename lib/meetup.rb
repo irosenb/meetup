20d758bfe5d170cd075e4da2cddc185175c798c5
Isaac Rosenberg <irosenb7@gmail.com>
2014-04-15 18:39:04 -0400

    refactoring

--- lib/meetup.rb
+++ lib/meetup.rb
@@ -3,6 +3,6 @@ require "httparty"
 
 module Meetup
   attr_accessor :key, :access_token
-
+  
   
 end
--- spec/meetup_spec.rb
+++ spec/meetup_spec.rb
@@ -1,15 +1,9 @@
 require "spec_helper.rb"
 
-describe Meetup::Client do
-  before do
-    @user = Meetup::Client.new("3a1b322e113b3130592c2f32193776a") 
-  end 
-
-  describe "#self" do
-    it "should return user's info" do
-      test = @user.self
-      puts test['details']
-      expect(test['city']).to eq("New York") 
+describe Meetup do
+  describe "#intialize" do
+    it "should return user's key" do
+      @user = Meetup.configure(:key => "3a1b322e113b3130592c2f32193776a")
     end
   end
 end
